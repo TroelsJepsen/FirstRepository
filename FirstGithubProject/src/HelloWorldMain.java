9e2368fd28ed6ac355531be04b2326855d75a4da
Unknown <troels101@gmail.com>
2017-10-05 13:23:42 +0200

    sdfg

--- FirstGithubProject/src/HelloWorldMain.java
+++ FirstGithubProject/src/HelloWorldMain.java
@@ -15,6 +15,8 @@ public class HelloWorldMain {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Goodbye world");
+        System.out.println("Hi again");
     }
     
 }

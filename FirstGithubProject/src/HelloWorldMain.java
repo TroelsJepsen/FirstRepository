c26715d3a884f805858f271b7a32b19835023c39
Unknown <troels101@gmail.com>
2017-10-05 13:13:54 +0200

    Main updated: "Goodbye World"

--- FirstGithubProject/src/HelloWorldMain.java
+++ FirstGithubProject/src/HelloWorldMain.java
@@ -14,7 +14,8 @@ public class HelloWorldMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");   
+        System.out.println("Hello World!");
+        System.out.println("Goodbye World!");
     }
     
 }
